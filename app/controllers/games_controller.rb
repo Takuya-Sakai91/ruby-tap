4ebb58d3b856da39c168c0873053f1c65fb677d7
Takuya Sakai <tkyntm0927@gmail.com>
2025-05-04 23:19:09 +0900

    コントローラーでランダムなRubyメソッドを取得できるようにした

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,6 +3,7 @@
 class GamesController < ApplicationController
   def show
     @game = Game.find(params[:id])
+    @ruby_methods = @game.ruby_methods
   end
 
   def new; end
@@ -12,9 +13,23 @@ class GamesController < ApplicationController
     @game.user = current_user if user_signed_in?
 
     if @game.save
+      # ランダムなRubyメソッドを追加
+      add_methods_to_game(@game)
       redirect_to game_path(@game)
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def add_methods_to_game(game)
+    # ランダムにRubyメソッドを取得
+    methods = RubyMethod.order('RANDOM()')
+
+    # ゲームにメソッドを追加
+    methods.each_with_index do |method, index|
+      game.game_methods.create(ruby_method: method, order: index)
     end
   end
 end

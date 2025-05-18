db0604ca8ee125f040aaacd28d7a900ec6d8cc1d
Takuya Sakai <tkyntm0927@gmail.com>
2025-05-18 09:51:53 +0900

    HomeControllerのindexアクションを更新し、ログイン状態に応じた遷移を追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  def index; end
+  def index
+    redirect_to user_path(current_user.id) if user_signed_in?
+  end
 end

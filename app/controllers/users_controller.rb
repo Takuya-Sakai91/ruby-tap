573f74c238ef20c244d54227aabebc4696d03cbc
Takuya Sakai <tkyntm0927@gmail.com>
2025-05-18 11:55:22 +0900

    UsersControllerでpreload(:games)を追加しN+1問題を解決

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show]
 
   def show
-    @user = User.find(params[:id])
+    @user = User.preload(:games).find(params[:id])
     redirect_to root_path, alert: I18n.t('users.access_denied') unless @user == current_user
   end
 end

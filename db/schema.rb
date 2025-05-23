288848ff5b1bf44d3524c7eb9d9369bece0c9411
Takuya Sakai <tkyntm0927@gmail.com>
2025-05-24 00:00:20 +0900

    usersにadminカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_17_133008) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_22_150755) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,6 +66,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_17_133008) do
     t.decimal "best_score", default: "0.0"
     t.decimal "previous_score", default: "0.0"
     t.string "username"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
